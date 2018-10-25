36ad94c923fc9437f31dde0cc167c35fe29f2b88
Lari Korpi <lari.korpi@tieto.com>
2018-10-25 22:53:11 +0300

    more advanced study

--- hello.cob
+++ hello.cob
@@ -1,5 +1,15 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. hello.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-NAME PIC A(30).
+       01 WS-ID PIC 9(5) VALUE '12345'.
+
        PROCEDURE DIVISION.
-       DISPLAY "Hello, world!".
+       A000-FIRST-PARA.
+       DISPLAY "My first Cobol!".
+       MOVE 'Hello' TO WS-NAME.
+           DISPLAY "My name is : "WS-NAME.
+           DISPLAY "My ID is : "WS-ID.
        STOP RUN.
